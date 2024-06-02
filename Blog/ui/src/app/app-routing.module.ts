8ccbc27e72584eb2731df0ca825fd0a35e32409b
mikhail.kachan <mikhail.kachan@afrans.by>
2024-06-02 17:35:04 +0300

    Added login and registation modules

--- Blog/ui/src/app/app-routing.module.ts
+++ Blog/ui/src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
+import { APP_ROUTES } from '@shared/constants/app-routes.const';
+
+const routes: Routes = [
+    {
+        path: APP_ROUTES.Login,
+        loadChildren: () => import('./login/login.module').then(x => x.LoginModule)
+    },
+    {
+        path: APP_ROUTES.SignUp,
+        loadChildren: () => import('./registration/registration.module').then(x => x.RegistrationModule)
+    },
+    {
+        path: '**',
+        redirectTo: APP_ROUTES.Login
+    }
+];
 @NgModule({
-    imports: [RouterModule.forRoot([])],
+    imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
 })
 export class AppRoutingModule {}
